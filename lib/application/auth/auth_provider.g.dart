8429804589920b7f20b6b2af3d5f500f4d21f172
1mos-droid <isemuk8@gmail.com>
2026-07-01 09:57:20 +0000

    refactor(auth_provider.g): finalize migration to Riverpod & clean architecture

--- lib/application/auth/auth_provider.g.dart
+++ lib/application/auth/auth_provider.g.dart
@@ -35,3 +35,26 @@ final authStateChangesProvider = AutoDisposeStreamProvider<User?>.internal(
       : _$authStateChangesHash,
   dependencies: null,
   allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AuthStateChangesRef = AutoDisposeStreamProviderRef<User?>;
+String _$authNotifierHash() => r'28b51a089dcd03d2324994226ae57aa9c833bb29';
+
+/// See also [AuthNotifier].
+@ProviderFor(AuthNotifier)
+final authNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<AuthNotifier, AppUser?>.internal(
+      AuthNotifier.new,
+      name: r'authNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$authNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$AuthNotifier = AutoDisposeAsyncNotifier<AppUser?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
