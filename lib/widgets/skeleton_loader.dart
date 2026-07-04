e194892e93fb87aa09c2c5d1921aef9c96d9b9bb
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:28 +0000

    refactor: update skeleton_loader.dart (part 1480/1500)

--- lib/widgets/skeleton_loader.dart
+++ lib/widgets/skeleton_loader.dart
@@ -14,3 +14,8 @@ class SkeletonContainer extends StatelessWidget {
     required this.height,
     this.borderRadius = 8,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final baseColor = isDark ? const Color(0xFF2E3142) : const Color(0xFFD8DEE9);
