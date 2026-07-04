483954efa5eaeb7089bde97f482e1dfdbe021984
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:19 +0000

    refactor: update sermon.dart (part 1296/1500)

--- lib/models/sermon.dart
+++ lib/models/sermon.dart
@@ -3,3 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Sermon {
   final String id;
   final String title;
+  final String speaker;
+  final String category; // Sermon, Media
+  final String duration;
+  final DateTime date;
