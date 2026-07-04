01857ab5fac4fa59a0186a5668c9d0852c805ffe
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:25 +0000

    feat: update modern_widgets.dart (part 1420/1500)

--- lib/widgets/modern_widgets.dart
+++ lib/widgets/modern_widgets.dart
@@ -138,3 +138,6 @@ class ContentCard extends StatelessWidget {
   });
 
   @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final isDark = theme.brightness == Brightness.dark;
