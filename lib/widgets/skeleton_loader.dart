94070c1de8354b66a9052bbefef8d270c9dea7f7
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:28 +0000

    style: update skeleton_loader.dart (part 1477/1500)

--- lib/widgets/skeleton_loader.dart
+++ lib/widgets/skeleton_loader.dart
@@ -2,3 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:rtc_mobile/theme/app_theme.dart';
+
+class SkeletonContainer extends StatelessWidget {
+  final double width;
+  final double height;
+  final double borderRadius;
