7b564cb864d098ef74d270722394ed69ac60c897
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:50:23 +0000

    refactor: update login_screen.dart (part 269/1500)

--- lib/ui/features/auth/login_screen.dart
+++ lib/ui/features/auth/login_screen.dart
@@ -175,10 +175,6 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                             width: 84,
                             height: 84,
                             padding: const EdgeInsets.all(12),
-                              ),
-                            ),
-                            Expanded(child: Divider(color: ObsidianTheme.borderHairline)),
-                          ],
                         ),
                         
                         const SizedBox(height: 16),
