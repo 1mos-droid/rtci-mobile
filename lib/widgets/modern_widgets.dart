628905c102aa3b14801c15856c791df7f878fb4d
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:26 +0000

    chore: update modern_widgets.dart (part 1440/1500)

--- lib/widgets/modern_widgets.dart
+++ lib/widgets/modern_widgets.dart
@@ -219,3 +219,7 @@ class ContentCard extends StatelessWidget {
                           bottom: 12,
                           right: 12,
                           child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                            decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.7),
+                              borderRadius: BorderRadius.circular(6),
