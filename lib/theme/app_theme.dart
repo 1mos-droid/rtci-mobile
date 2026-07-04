a8e62ee0bd1ca4cd70e8df1d7558b4afbf20fab6
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:50:16 +0000

    chore: update app_theme.dart (part 127/1500)

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -175,8 +175,8 @@ class AppTheme {
           minimumSize: const Size.fromHeight(54),
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
           elevation: 0,
-          textStyle: GoogleFonts.inter(
-            fontWeight: FontWeight.w600,
+          textStyle: _getTextStyle(
+            fontWeight: FontWeight.w700,
             fontSize: 17,
             letterSpacing: -0.4,
           ),
