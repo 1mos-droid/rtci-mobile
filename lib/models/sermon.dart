f711667eb9b55d05c4ad4769e9d4a2875e2941d3
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:19 +0000

    docs: update sermon.dart (part 1304/1500)

--- lib/models/sermon.dart
+++ lib/models/sermon.dart
@@ -35,3 +35,7 @@ class Sermon {
       'duration': duration,
       'date': FieldValue.serverTimestamp(),
       'image_url': imageUrl,
+      'tag': tag,
+      'audio_url': audioUrl,
+      'video_url': videoUrl,
+      'logged_by': loggedBy,
