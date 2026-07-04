eea1cb96ab2c90a1ca2bfca0455109babfc4de31
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:20 +0000

    style: update startup_loader_screen.dart (part 1332/1500)

--- lib/ui/features/auth/startup_loader_screen.dart
+++ lib/ui/features/auth/startup_loader_screen.dart
@@ -1,3 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rtc_mobile/theme/app_theme.dart';
+
+class StartupLoaderScreen extends StatelessWidget {
