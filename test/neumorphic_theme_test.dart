23c6f3e3c6e3e3239a6fb9a5792b5b8ac148ef72
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:29 +0000

    chore: update neumorphic_theme_test.dart (part 1489/1500)

--- test/neumorphic_theme_test.dart
+++ test/neumorphic_theme_test.dart
@@ -3,3 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rtc_mobile/theme/app_theme.dart';
 import 'package:rtc_mobile/ui/features/auth/startup_loader_screen.dart';
+
+void main() {
+  setUpAll(() {
+    GoogleFonts.config.allowRuntimeFetching = false;
+  });
