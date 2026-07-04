4ed44368ea568e40c53985ff2e4901fcd76b1f91
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:50:29 +0000

    feat: update signup_screen.dart (part 376/1500)

--- lib/ui/features/auth/signup_screen.dart
+++ lib/ui/features/auth/signup_screen.dart
@@ -131,10 +131,6 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
                                   height: 1.4,
                                 ),
                               ).animate().fadeIn(delay: 100.ms, duration: 500.ms).slideY(begin: 0.2, end: 0, curve: Curves.easeOutQuad),
-                              const SizedBox(height: 16),
-
-                              TextFormField(
-                                controller: _passwordController,
                                 obscureText: !_showPassword,
                                 style: TextStyle(color: ObsidianTheme.textVibrant),
                                 decoration: InputDecoration(
