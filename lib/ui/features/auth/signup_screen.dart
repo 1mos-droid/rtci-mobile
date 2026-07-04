93f379d28ec5689f623ef91bb3b4fe25ee8d17c2
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:50:33 +0000

    style: update signup_screen.dart (part 459/1500)

--- lib/ui/features/auth/signup_screen.dart
+++ lib/ui/features/auth/signup_screen.dart
@@ -345,6 +345,10 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
                                     Navigator.pushReplacement(
                                       context,
                                       MaterialPageRoute(builder: (_) => const LoginScreen()),
+                                    );
+                                  },
+                                  child: Text(
+                                    "Already have an account? Sign In",
                             ],
                           ),
                         ).animate().fadeIn(delay: 200.ms, duration: 600.ms).scaleXY(begin: 0.95, end: 1.0, curve: Curves.easeOutBack).slideY(begin: 0.1, end: 0, curve: Curves.easeOutQuad),
