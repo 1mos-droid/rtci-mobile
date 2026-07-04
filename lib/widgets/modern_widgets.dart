cdeb15648ccae6a707db587a3e8a157e4ca2e0aa
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:23 +0000

    refactor: update modern_widgets.dart (part 1386/1500)

--- lib/widgets/modern_widgets.dart
+++ lib/widgets/modern_widgets.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomPrimaryButton extends StatefulWidget {
+  final String text;
+  final VoidCallback? onPressed;
+  final bool isLoading;
+  final IconData? icon;
