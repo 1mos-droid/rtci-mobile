aa71bba67ea3f5793e750bd6ba60805cd1f45a1a
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:20 +0000

    refactor: update startup_loader_screen.dart (part 1333/1500)

--- lib/ui/features/auth/startup_loader_screen.dart
+++ lib/ui/features/auth/startup_loader_screen.dart
@@ -5,3 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:rtc_mobile/theme/app_theme.dart';
 
 class StartupLoaderScreen extends StatelessWidget {
+  final String message;
+
+  const StartupLoaderScreen({
+    super.key,
