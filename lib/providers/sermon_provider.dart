18a5270e19e88c737713fce8fa2b558338a4f879
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:51:19 +0000

    chore: update sermon_provider.dart (part 1311/1500)

--- lib/providers/sermon_provider.dart
+++ lib/providers/sermon_provider.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rtc_mobile/models/sermon.dart';
+
+class SermonProvider extends ChangeNotifier {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
