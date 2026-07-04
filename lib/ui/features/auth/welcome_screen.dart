55adf2148d2b2e1ad0106bb674fae08a88251683
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:50:37 +0000

    fix: update welcome_screen.dart (part 544/1500)

--- lib/ui/features/auth/welcome_screen.dart
+++ lib/ui/features/auth/welcome_screen.dart
@@ -185,10 +185,6 @@ class WelcomeScreen extends StatelessWidget {
                     ],
                   ),
                 ).animate().fadeIn(delay: 600.ms, duration: 600.ms).slideY(begin: 0.1, end: 0, curve: Curves.easeOutQuad),
-                      padding: const EdgeInsets.all(14.0),
-                      child: Image.asset(
-                        'assets/church_logo.png',
-                        fit: BoxFit.contain,
                       ),
                     ),
                   ),
