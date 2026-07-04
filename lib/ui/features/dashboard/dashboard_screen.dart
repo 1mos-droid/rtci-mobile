48ed5d936cf4b3ee2efba510334d93ce442c5be6
1mos-droid <isemuk8@gmail.com>
2026-07-04 17:50:54 +0000

    refactor: update dashboard_screen.dart (part 865/1500)

--- lib/ui/features/dashboard/dashboard_screen.dart
+++ lib/ui/features/dashboard/dashboard_screen.dart
@@ -1184,10 +1184,6 @@ class _DashboardScreenState extends ConsumerState<DashboardScreen> {
           }
         );
       },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
                   event.name,
                   style: GoogleFonts.plusJakartaSans(
                     fontSize: 16,
